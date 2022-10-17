7192c3785f6891253cc5170d20283b0133a3bf9d
Rima <rimamanaseryan99@gmail.com>
2022-10-17 17:22:19 +0400

    Add project files.

---
+++ FilteringApplication/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Data.Entity;
+
+namespace FilteringApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        List<User> group = new List<User>()
+        {
+            new User("Aram", "Gevorgyan", 18, Courses.Css),
+            new User("Sveta", "Martirosyan", 21, Courses.JavaScript),
+            new User("Gexam", "Vardanyan", 19, Courses.FSharp),
+            new User("Vardan", "Gexamyan", 25, Courses.CSharp)
+        };
+        
+        [HttpPost("Filter")]
+        public async Task<IActionResult> FilterUsers([FromBody] User testi)
+        {
+            List<User> FilteredUsers = group;
+            if (!String.IsNullOrEmpty(testi.Name))
+                FilteredUsers = FilteredUsers.Where(u => u.Name == testi.Name.ToLower()).ToList();
+            if (!String.IsNullOrEmpty(testi.LastName))
+                FilteredUsers = FilteredUsers.Where(u => u.LastName == testi.LastName.ToLower()).ToList();
+            if (testi.Age.HasValue)
+                FilteredUsers = FilteredUsers.Where(u => u.Age == testi.Age).ToList();
+            if (testi.Course.HasValue)
+                FilteredUsers = FilteredUsers.Where(u => u.Course == testi.Course).ToList();
+           
+            var response = await FilteredUsers.AsQueryable().ToListAsync();
+            return Ok(response);
+        }
+    }
+}
---
+++ FilteringApplication/User.cs
@@ -0,0 +1,28 @@
+﻿namespace FilteringApplication
+{
+    public class User
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+        public Courses? Course { get; set; }
+        public User() { }
+        public User(string name, string lname, int age, Courses course)
+        {
+            Name = name;
+            LastName = lname;
+            Age = age;
+            Course = course;
+        }
+    }
+
+    public enum Courses
+    {
+        CSharp,
+        JavaScript,
+        Html,
+        Css,
+        FSharp
+    }
+}
+
